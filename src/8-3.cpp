2c2cc3330c41347e386f167a3ce6a8cc36c6799d
Jasper Young <jasper.c.young@gmail.com>
2025-05-19 14:29:11 -0400

    Add solution for 8-2 to 8-3

---
+++ src/8-3.cpp
@@ -0,0 +1,10 @@
+using uc = unsigned char;
+using cuc = const uc;
+using pi = int *;
+using ppc = char **;
+using pac = char (*)[];
+using a7pi = int *[7];
+using pa7pi = a7pi *;
+using a8a7pi = a7pi[8];
+
+int main() {};
